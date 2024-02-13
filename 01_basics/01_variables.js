c899f3b2f1e0ada4bb2d76942bc91883c11d0648
Upashana Baruah <91074059+baruahupu@users.noreply.github.com>
2024-02-13 06:42:05 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 338
+var accountName = "upashana"
+let accountEmail = "upashana@gmail.com"
+accountCity = "Guwahati"
+let accountState //undefined
+let temp = null //null type
+
+// accountId = 2 //not allowed as it is a const variable
+
+console.table([accountId, accountName, accountEmail, accountCity])
+
