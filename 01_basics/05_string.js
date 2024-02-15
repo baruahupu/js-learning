544e3c6e76ff401f4dff75c54ef1fe4f964d5548
Upashana Baruah <91074059+baruahupu@users.noreply.github.com>
2024-02-15 05:04:03 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+//string interpolation
+
+let name = "upashana"
+let age = 27
+console.log(`Hi, my name is ${name} and my age is ${age}`)
+
+const anotherName = new String('Raktim') //it creates a string object and reprsent the value in key=>value pair
+
+console.log(anotherName.length) //length of string
+console.log(anotherName.toUpperCase()) //convert to upper case
+console.log(anotherName.charAt(2)) //to find the character by index
+console.log(anotherName.length)
+
