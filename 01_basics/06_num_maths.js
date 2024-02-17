c7374848589aa1e957b982a0b8a21a4d065309fe
Upashana Baruah <91074059+baruahupu@users.noreply.github.com>
2024-02-17 10:13:52 +0000

    math function

--- 01_basics/06_num_maths.js
+++ 01_basics/06_num_maths.js
@@ -17,6 +17,10 @@ console.log(Math.ceil(4.2))
 console.log(Math.floor(4.6))
 console.log(Math.min(-4, 5, 7, 3)) //find min value from array
 console.log(Math.max(-4, 6, 5, 9)) //find max value from array
+console.log(Math.exp(1)) //it returns e raised to the power of a number.
+console.log(Math.pow(7, 3)) //it returns the value of a base raised to a power.
+console.log(Math.sqrt(4, 2)) //static method returns the square root of a number.
+console.log(Math.trunc(14.4662))//it returns the integer part of a number by removing any fractional digits.
 
 console.log((Math.random() * 10) + 1)
 
