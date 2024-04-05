5df18dcb01e26a232e49a4d28c26adfff6a04e6e
Upashana Baruah <91074059+baruahupu@users.noreply.github.com>
2024-04-05 15:56:18 +0000

    object

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,3 +1,5 @@
+//object literals
+
 const mySym = Symbol("key 1")
 
 const jsUser = {
@@ -31,3 +33,39 @@ Object.freeze(jsUser) // it will freeze the values of the key so that the values
 jsUser.name = "user 3"
 // console.log(jsUser)
 
+
+//singleton object
+
+const myObj = new Object() //when an object declared through a constructor
+
+myObj.name = "Object User"
+myObj.email = "ouser@gmail.com"
+myObj.isLoggedIn = false
+
+console.log(myObj)
+
+const regularUser = {
+    name: "user",
+    fullname: {
+        userfullname: {
+            firstname: "upashana",
+            lastname: "baruah"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname) //object within object can be access using . operator
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+// const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1, obj2) // assign(target, ...source), more than 1 source can be added
+console.log(obj3)
+const obj4 = {...obj1, ...obj2} //spread operator can be used to merge multiple arrays
+console.log(obj4) 
+
+console.log(Object.keys(myObj)) //it gives all the keys in the object in an array
+console.log(Object.values(myObj)) //it gives all the values in the object in an array
+console.log(Object.entries(myObj)) //it gives all the key:values pairs as array in the object
+console.log(myObj.hasOwnProperty("isLoggedIn")) //it gives all the key:values pairs as array in the object
